6d4042b178ad43b6e15a2d63513bab4d70f915c4
heniotierra <heniotster@gmail.com>
2019-02-05 23:43:03 -0300

    Update tboplayer.py

--- tboplayer.py
+++ tboplayer.py
@@ -2255,7 +2255,7 @@ class AutoLyrics(Toplevel):
 # ***************************************
 
 if __name__ == "__main__":
-    datestring="9 Jan 2019"
+    datestring="5 Feb 2019"
 
     dbusif_tboplayer = None
     try:
